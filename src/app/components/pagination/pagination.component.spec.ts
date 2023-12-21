c23c1f27f4baadc4bcfad36d08c5437afd9bd0b8
Carolina Moreno <carolina.moreno.v@ug.uchile.cl>
2023-12-21 13:38:37 -0300

    Test pagination

--- src/app/components/pagination/pagination.component.spec.ts
+++ src/app/components/pagination/pagination.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { EventEmitter } from '@angular/core';
 
 import { PaginationComponent } from './pagination.component';
 
@@ -8,10 +10,10 @@ describe('PaginationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PaginationComponent ]
-    })
-    .compileComponents();
-
+      declarations: [PaginationComponent],
+      imports: [MatPaginatorModule],
+    }).compileComponents();
+  
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,43 @@ describe('PaginationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit new page event when onPageChanged is called', () => {
+    // Arrange
+    spyOn(component.newPageEvent, 'emit');
+
+    const pageEvent: PageEvent = {
+      pageIndex: 2,
+      pageSize: 20,
+      length: 100,
+    };
+
+    // Act
+    component.onPageChanged(pageEvent);
+
+    // Assert
+    expect(component.pageIndex).toEqual(2);
+    expect(component.pageSize).toEqual(20);
+    expect(component.newPageEvent.emit).toHaveBeenCalledWith(3); // pageIndex + 1
+  });
+
+  it('should have default values for pageSize and pageIndex', () => {
+    // Assert
+    expect(component.pageSize).toEqual(20);
+    expect(component.pageIndex).toEqual(0);
+  });
+
+  it('should update pageIndex and pageSize when input values change', () => {
+    // Arrange
+    const newNumOfPages = 50;
+
+    // Act
+    component.numOfPages = newNumOfPages;
+    fixture.detectChanges();
+
+    // Assert
+    expect(component.pageSize).toEqual(20); // unchanged
+    expect(component.pageIndex).toEqual(0); // unchanged
   });
 });
--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 
 @Component({
@@ -7,7 +7,7 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrls: ['./pagination.component.css'],
 })
 
-export class PaginationComponent implements OnInit, OnDestroy {
+export class PaginationComponent implements OnInit, OnDestroy, OnChanges {
   @Input() numOfPages!: number;
   @Output() newPageEvent = new EventEmitter<number>();
 
@@ -18,14 +18,13 @@ export class PaginationComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {}
 
-  ngOnDestroy(): void {
-    
-  }
+  ngOnDestroy(): void {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    
+    // Handle changes to numOfPages if needed
   }
- // emitir eventos con el n° de página 
+
+  // Emitir eventos con el n° de página
   onPageChanged(event: PageEvent): void {
     this.pageIndex = event.pageIndex;
     this.pageSize = event.pageSize;
